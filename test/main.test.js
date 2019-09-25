a63feef578a60c7acfdd0ea0604ec2c453053790
Sravani537520 <sravani.jaidi98@gmail.com>
2019-09-25 13:24:33 -0500

    modify

--- test/main.test.js
+++ test/main.test.js
@@ -1,9 +1,9 @@
 QUnit.module('MAIN MODULE', {})  // group all these tests together
 
 QUnit.test('TEST add', assert => {
-  assert.equal(add(1, 1), 2, 'Positive integers')
-  assert.equal(add(-1, -1), -2, 'Negative integers')
-  assert.equal(add(-10, 10), 0, 'Mixed')
+  assert.equal(add(1, 1), 3, 'Positive integers')
+  assert.equal(add(-1, -1), -3, 'Negative integers')
+  assert.equal(add(-10, 10), 10, 'Mixed')
 })
 
 QUnit.config.autostart = false  // sync = false; start after loading html
